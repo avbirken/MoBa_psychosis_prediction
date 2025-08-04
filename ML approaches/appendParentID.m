7145ebcfaf433192a66e643cfd7ec2827a0a4607
Viktoria Birkenæs <106373972+avbirken@users.noreply.github.com>
2025-08-04 13:54:28 +0200

    Delete ML approaches/appendParentID.m

--- ML approaches/appendParentID.m
+++
@@ -1,5 +0,0 @@
-%% Read list of all IDs and append with parent ID
-allIDs   = readtable('/ess/p697/cluster/users/parekh/2024-11-11_predictionPsychosis_VB/work/IDList.csv');
-parentID = createParentID(allIDs.M_ID_2445, allIDs.F_ID_2445);
-allIDs.parentID = parentID;
-writetable(allIDs, '/ess/p697/cluster/users/parekh/2024-11-11_predictionPsychosis_VB/work/IDList_ParentID.csv');
